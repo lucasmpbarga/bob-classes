9b044606e47e762bda947f6e214c0de8e269daac
Lucas Barga <lucasmpbarga@gmail.com>
2022-10-21 21:18:28 -0300

    add tap z

--- contacts-project/contacts-project-v5.0.js
+++ contacts-project/contacts-project-v5.0.js
@@ -75,15 +75,16 @@ const updateContact = (index, attribute, newValue) => {
 
 const renderMenu = () => {
   console.clear();
-  console.log("#######################");
+  console.log("#########################");
   console.log("#### Contacts Project ###");
-  console.log("#######################");
+  console.log("#########################");
   console.log("1. Create new contact.");
   console.log("2. List all contacts.");
   console.log("3. Show one contact by index.");
   console.log("4. Delete contact by index.");
   console.log("5. Update contact by index.");
-  console.log("#######################");
+  console.log("#########################");
+  console.log("Tap Z to exit....");
   spaceLine();
   option = prompt("Select your option: ");
 };
