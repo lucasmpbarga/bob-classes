3bc39ef88fe14d229d116625c9151bca98cc42d4
Lucas Barga <lucasmpbarga@gmail.com>
2022-10-23 16:22:32 -0300

    v7

---
+++ contacts-project/contacts-project-v7.0.js
@@ -0,0 +1,8 @@
+// v7
+// criar uma tela em HTML e CSS, com ou sem uso de framework
+// apenas visual que permita
+// cadastrar novos contatos
+// exibir informacoes dos contatos
+// deletar um contato
+// atualizar um contato
+// apenas visual nao precisamos manipular dados nesse primeiro momento
