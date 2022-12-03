8253fae8a47589ccdf496f010aeef681ecdb3488
Lucas Barga <lucas.barga.ext@dasa.com.br>
2022-12-03 17:00:52 -0300

    v11 done

---
+++ contacts-project/v11.0/packages/front/src/service/contact-service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3002",
+});
+
+const createContact = (contact) => {
+  instance.post("/contacts", contact);
+};
+
+const readContacts = () => {
+  return instance.get("/contacts");
+};
+
+const updateContact = (id, contact) => {
+  instance.put(`/contacts/${id}`, contact);
+};
+
+const deleteContact = (contactId) => {
+  instance.delete(`/contacts/${contactId}`);
+};
+
+export default {
+  createContact,
+  readContacts,
+  updateContact,
+  deleteContact,
+};
